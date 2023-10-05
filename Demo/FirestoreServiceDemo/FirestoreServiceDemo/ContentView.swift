c7fcca4e22cea04f677663d7ad7c65142b44aaa9
Eyup Yasuntimur <yasuntimure@gmail.com>
2023-10-05 12:53:20 -0700

    Demo target added

---
+++ Demo/FirestoreServiceDemo/FirestoreServiceDemo/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  FirestoreServiceDemo
+//
+//  Created by Eyüp on 2023-10-05.
+//
+
+import SwiftUI
+import FirestoreService
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo/FirestoreServiceDemo/FirestoreServiceDemo/FirestoreServiceDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirestoreServiceDemoApp.swift
+//  FirestoreServiceDemo
+//
+//  Created by Eyüp on 2023-10-05.
+//
+
+import SwiftUI
+
+@main
+struct FirestoreServiceDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
